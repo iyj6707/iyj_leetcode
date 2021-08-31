03431be0076758991253bff98a06e7027bf95b74
Yeonjae Im <iyj6707@naver.com>
2021-08-31 22:35:15 +0900

    Time: 0 ms (100.00%), Space: 38.1 MB (93.60%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,12 +1,25 @@
 class Solution {
     public int findMin(int[] nums) {
-        int before = nums[0];
-        for (var num: nums) {
-            if (before > num) {
-                return num;
-            }
-            before = num;
+        int left = 0;
+        int right = nums.length - 1;
+        if (nums.length == 1) {
+            return nums[0];
         }
-        return nums[0];
+        
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+
+        while(true) {
+            int mid = (left + right) / 2;
+            if (nums[left] < nums[mid]) {
+                left = mid;
+            } else if (nums[left] > nums[mid]) {
+                right = mid;
+            } else {
+                break;
+            }
+        }
+        return nums[left + 1];
     }
 }
