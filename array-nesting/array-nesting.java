68ca412b2e280c4103b4276ea9152ae24ea06dba
Yeonjae Im <iyj6707@naver.com>
2021-09-01 17:00:11 +0900

    Time: 5 ms (31.93%), Space: 45.5 MB (17.52%) - LeetHub

---
+++ array-nesting/array-nesting.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int arrayNesting(int[] nums) {
+        int len = nums.length;
+        int answer = 0;
+        
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == i) {
+                answer = Math.max(answer, 1);
+                continue;
+            }
+            
+            int iterLen = 0;
+            if (nums[i] != len) {
+                int iter = nums[i];
+                while (true) {
+                    int tmp = iter;
+                    iter = nums[iter];
+                    nums[tmp] = len;
+                    if (iter == len) {
+                        break;
+                    }
+                    iterLen++;
+                }   
+            }
+            answer = Math.max(answer, iterLen);
+            if (answer > Math.ceil(len / 2.0)) {
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
