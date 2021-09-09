b9cda9b24a3576fc79a4751d8d90436a8bb9f0ab
Yeonjae Im <iyj6707@naver.com>
2021-09-09 13:15:09 +0900

    Time: 100 ms (10.40%), Space: 105.8 MB (5.11%) - LeetHub

--- shifting-letters/shifting-letters.java
+++ shifting-letters/shifting-letters.java
@@ -1,16 +1,37 @@
 class Solution {
+//     public String shiftingLetters(String s, int[] shifts) {
+//         StringBuilder sb = new StringBuilder();
+//         int cnt = 0;
+//         int charNum = 'z' - 'a' + 1;
+        
+//         for (int i = shifts.length - 1; i >= 0; i--) {
+//             char ch = s.charAt(i);
+//             cnt = (cnt + shifts[i]) % charNum;
+            
+//             sb.append((char) (ch + cnt > 'z' ? ch + cnt - charNum : ch + cnt));
+//         }
+        
+//         return sb.reverse().toString();
+//     }
+    
     public String shiftingLetters(String s, int[] shifts) {
         StringBuilder sb = new StringBuilder();
-        int cnt = 0;
         int charNum = 'z' - 'a' + 1;
         
-        for (int i = shifts.length - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-            cnt = (cnt + shifts[i]) % charNum;
-            
-            sb.append((char) (ch + cnt > 'z' ? ch + cnt - charNum : ch + cnt));
+        for (int i = shifts.length - 2; i >= 0; i--) {
+            shifts[i + 1] %= charNum;
+            shifts[i] += shifts[i + 1];
         }
         
-        return sb.reverse().toString();
+        System.out.println(Arrays.toString(shifts));
+        
+        for (int i = 0; i < shifts.length; i++) {
+            char ch = s.charAt(i);
+            shifts[i] %= charNum;
+            
+            sb.append((char) (ch + shifts[i] > 'z' ? ch + shifts[i] - charNum : ch + shifts[i]));
+        }
+        
+        return sb.toString();
     }
 }
