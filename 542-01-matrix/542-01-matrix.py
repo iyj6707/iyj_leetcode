48098d57ec4b3b0c7b0c50d06e94f883636059dc
Yeonjae Im <iyj6707@naver.com>
2022-04-19 20:12:28 +0900

    Time: 775 ms (68.27%), Space: 17.3 MB (43.33%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -10,22 +10,13 @@ class Solution:
                     queue.append((x, y))
                     visited.add((x, y))
 
-        count = len(queue)
-        step = 1
         move_dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         while queue:
-            new_count = 0 
-            while count > 0:
-                x, y = queue.popleft()
-                count -= 1                
-                for move_dir in move_dirs:
-                    dx = x + move_dir[0]
-                    dy = y + move_dir[1]
-                    if 0 <= dx < num_rows and 0 <= dy < num_cols and mat[dx][dy] != 0 and (dx, dy) not in visited: 
-                        new_count += 1
-                        visited.add((dx, dy))
-                        mat[dx][dy] = step
-                        queue.append((dx, dy))
-            count = new_count  
-            step += 1
+            x, y = queue.popleft()     
+            for move_dir in move_dirs:
+                dx, dy = x + move_dir[0], y + move_dir[1]
+                if 0 <= dx < num_rows and 0 <= dy < num_cols and mat[dx][dy] != 0 and (dx, dy) not in visited: 
+                    mat[dx][dy] = mat[x][y] + 1
+                    visited.add((dx, dy))
+                    queue.append((dx, dy))
         return mat
