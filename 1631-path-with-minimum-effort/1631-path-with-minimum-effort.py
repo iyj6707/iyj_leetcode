6431a8ddaa8869eb35a62584515521bd9d0680f9
Yeonjae Im <iyj6707@naver.com>
2022-04-28 09:47:19 +0900

    Time: 4612 ms (10.33%), Space: 25.2 MB (5.17%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -10,14 +10,14 @@ class Solution:
                     if abs(heights[x][y] - heights[dx + x][dy + y]) <= LIMIT:
                         dfs(LIMIT, dx + x, dy + y)
         
-        beg, end = -1, max(max(heights, key = max))
-        while beg + 1 < end:
+        beg, end = 0, max(max(heights, key = max))
+        while beg < end:
             mid = (beg + end) // 2
             visited = set()
             dfs(mid, 0, 0)
             if (m - 1, n - 1) in visited:
                 end = mid
             else:
-                beg = mid
+                beg = mid + 1
                 
         return end
