093d6f6ba8bac4c707d437b9c4e2ce342df4dbb3
Yeonjae Im <iyj6707@naver.com>
2022-04-30 14:23:18 +0900

    Time: 51 ms (31.25%), Space: 14 MB (21.75%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -6,11 +6,8 @@ class Solution:
             return 0
         elif len(nums) == 1:
             return(nums[0])
-        elif len(nums) == 2:
-            return(max(nums[0], nums[1]))
         
         dp[1] = nums[0]
-        dp[2] = max(nums[0], nums[1])
         
         for idx in range(2, len(nums) + 1):
             dp[idx] = max(nums[idx-1] + dp[idx-2], dp[idx-1])
