92af42afceaf2c36f85cd2ea61c196cc858403a4
Yeonjae Im <iyj6707@naver.com>
2022-09-25 22:15:54 +0900

    Time: 2 ms (61.65%), Space: 41.4 MB (78.40%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -6,21 +6,18 @@ class Solution {
             return List.of(1);
         }
         
-        for(int i=0; i<=rowIndex; i++){
+        pre.add(1);
+        
+        for(int i=1; i<=rowIndex; i++){
             List<Integer> curr = new ArrayList<>();
             
-            if(i == 0){
-                curr.add(1);
-                pre.add(1);
+            curr.add(1);
+            for(int j=1; j<i; j++){
+                curr.add(pre.get(j-1)+pre.get(j));
             }
-            else{
-                curr.add(1);
-                for(int j=1; j<i; j++){
-                    curr.add(pre.get(j-1)+pre.get(j));
-                }
-                curr.add(1);
-                pre = curr;
-            }
+            curr.add(1);
+            pre = curr;
+
             if(i == rowIndex){
                 return pre;
             }
